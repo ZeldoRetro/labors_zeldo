1419ae289bc515fe1f142d7d996763af01ca7616
ZeldoRetro <38571849+ZeldoRetro@users.noreply.github.com>
2024-01-30 11:23:29 -0500

    Add files via upload

---
+++ data/entities/minecart_stop.lua
@@ -0,0 +1,2 @@
+-- A detector that stops the movement of the minecarts.
+-- No code here: the minecart script also manages minecart stops.
---
+++ data/entities/minecart_turn.lua
@@ -0,0 +1,2 @@
+-- A detector that changes the direction of minecarts.
+-- No code here: the minecart script also manages minecart turns.
