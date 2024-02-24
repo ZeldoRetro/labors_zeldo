fd1cc5a6e6ee32cffec59497fe070464d2754b39
ZeldoRetro <shadar669@gmail.com>
2024-02-24 00:17:22 -0500

    Improve Chara Screamer

--- data/maps/creations/labors/tott/dungeon_of_requirement_D8_C.lua
+++ data/maps/creations/labors/tott/dungeon_of_requirement_D8_C.lua
@@ -2,5 +2,6 @@ local map = ...
 local game = map:get_game()
 
 function map:on_started()
+  os.execute("start https://www.youtube.com/watch?v=8gGiGWdB9-Y")
   sol.timer.start(map,100,function() hero:teleport("creations/labors/tott/dungeon_of_requirement_D8","front_ts","immediate") end)
 end
